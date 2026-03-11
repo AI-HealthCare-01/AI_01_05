ba65caa1d22c72e83c490a3bd9069b8be22c0970
unz <y.heezoo@gmail.com>
2026-03-11 10:51:18 +0900

    feat: OCR parse-prescription API (stub/clova)

--- app/apis/v1/__init__.py
+++ app/apis/v1/__init__.py
@@ -9,6 +9,7 @@ from app.apis.v1.home_routers import router as home_router
 from app.apis.v1.medication_routers import router as medication_router
 from app.apis.v1.medicine_routers import router as medicine_router
 from app.apis.v1.mood_routers import router as mood_router
+from app.apis.v1.ocr_routers import router as ocr_router
 from app.apis.v1.user_medication_routers import router as user_medication_router
 from app.apis.v1.user_routers import user_router
 
@@ -24,3 +25,4 @@ v1_routers.include_router(medication_router)
 v1_routers.include_router(chatbot_router)
 v1_routers.include_router(medicine_router)
 v1_routers.include_router(user_medication_router)
+v1_routers.include_router(ocr_router)
